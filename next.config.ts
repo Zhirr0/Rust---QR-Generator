d507609eb8e7ef4458f14949c76d92199257c12b
Zhir Bakhtyar <zheerbaxtyar9@gmail.com>
2026-04-02 19:11:12 +0300

    fix: maybe this one will fix it

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack(config) {
+  turbopack: {},
+  webpack(config, { isServer }) {
     config.experiments = {
       ...config.experiments,
       asyncWebAssembly: true,
       layers: true,
     };
+
+    if (isServer) {
+      config.output.webassemblyModuleFilename =
+        "./../static/wasm/[modulehash].wasm";
+    }
+
     return config;
   },
 };
