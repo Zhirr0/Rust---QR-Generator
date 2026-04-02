cf6f6aefc269d022386fff3c03792b811be22834
Zhir Bakhtyar <zheerbaxtyar9@gmail.com>
2026-04-02 19:24:27 +0300

    fix: maybe this will make the log work and return no errors

--- vercel-install.sh
+++ vercel-install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- -y
-export PATH="/root/.cargo/bin:$PATH"
+# Vercel has Rust pre-installed but without the wasm target
+# Just add it directly using rustup which is also available
+rustup target add wasm32-unknown-unknown
 
+# Download wasm-pack prebuilt binary
 curl -L "https://github.com/rustwasm/wasm-pack/releases/download/v0.13.1/wasm-pack-v0.13.1-x86_64-unknown-linux-musl.tar.gz" -o wasm-pack.tar.gz
 tar xf wasm-pack.tar.gz --wildcards --no-anchored 'wasm-pack' --strip-components=1
 rm wasm-pack.tar.gz
